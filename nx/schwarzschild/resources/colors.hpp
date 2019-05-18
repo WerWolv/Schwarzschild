7748be21ec08d1d9944189a703842a5c5cc1c1b8
WerWolv <werwolv98@gmail.com>
2019-05-19 00:46:49 +0200

    Made switch colors SDL_Colors

--- nx/schwarzschild/resources/colors.hpp
+++ nx/schwarzschild/resources/colors.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
-namespace schwarzschild::resources {
-    enum SwitchColors {
-        Color_Dark_Background    = 0x2D2D2DFF,
-        Color_Dark_Panel         = 0x323232FF,
-        Color_Dark_Text          = 0xFFFFFFFF,
-        Color_Dark_Glowing       = 0x1ABCFDFF,
-        Color_Dark_Selected      = 0x16C9C5FF,
-        Color_Dark_Selected_Text = 0x00FFCAFF,
+#include <switch.h>
+#include <SDL.h>
 
-        Color_Light_Background    = 0xEBEBEBFF,
-        Color_Light_Panel         = 0xF0F0F0FF,
-        Color_Light_Text          = 0x2D2D2DFF,
-        Color_Light_Glowing       = 0x00FFCAFF,
-        Color_Light_Selected      = 0x16C9C5FF,
-        Color_Light_Selected_Text = 0x3250F0FF
-    };
+namespace schwarzschild::resources {
+    namespace SwitchColors {
+        SDL_Color Color_Dark_Background     = { 0x2D, 0x2D, 0x2D, 0xFF };
+        SDL_Color Color_Dark_Panel          = { 0x32, 0x32, 0x32, 0xFF };
+        SDL_Color Color_Dark_Text           = { 0xFF, 0xFF, 0xFF, 0xFF };
+        SDL_Color Color_Dark_Glowing        = { 0x1A, 0xBC, 0xFD, 0xFF };
+        SDL_Color Color_Dark_Selected       = { 0x16, 0xC9, 0xC5, 0xFF };
+        SDL_Color Color_Dark_Selected_Text  = { 0x00, 0xFF, 0xCA, 0xFF };
+        SDL_Color Color_Light_Background    = { 0xEB, 0xEB, 0xEB, 0xFF };
+        SDL_Color Color_Light_Panel         = { 0xF0, 0xF0, 0xF0, 0xFF };
+        SDL_Color Color_Light_Text          = { 0x2D, 0x2D, 0x2D, 0xFF };
+        SDL_Color Color_Light_Glowing       = { 0x00, 0xFF, 0xCA, 0xFF };
+        SDL_Color Color_Light_Selected      = { 0x16, 0xC9, 0xC5, 0xFF };
+        SDL_Color Color_Light_Selected_Text = { 0x32, 0x50, 0xF0, 0xFF };
+    }
 }
