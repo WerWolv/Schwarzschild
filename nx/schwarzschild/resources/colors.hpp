b343472eb03d84686d38e108dcd250c6dc1ddb43
Ac_K <Acoustik666@gmail.com>
2019-05-18 18:46:46 +0200

    Add standard color schemes

--- nx/schwarzschild/resources/colors.hpp
+++ nx/schwarzschild/resources/colors.hpp
@@ -4,6 +4,18 @@
 
 namespace schwarzschild::resources {
     enum SwitchColors {
+        Color_Dark_Background    = 0x2D2D2DFF,
+        Color_Dark_Panel         = 0x323232FF,
+        Color_Dark_Text          = 0xFFFFFFFF,
+        Color_Dark_Glowing       = 0x1ABCFDFF,
+        Color_Dark_Selected      = 0x16C9C5FF,
+        Color_Dark_Selected_Text = 0x00FFCAFF,
 
+        Color_Light_Background    = 0xEBEBEBFF,
+        Color_Light_Panel         = 0xF0F0F0FF,
+        Color_Light_Text          = 0x2D2D2DFF,
+        Color_Light_Glowing       = 0x00FFCAFF,
+        Color_Light_Selected      = 0x16C9C5FF,
+        Color_Light_Selected_Text = 0x3250F0FF
     };
 }
